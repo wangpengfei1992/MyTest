52de25dc0ad5b3dafb0e0564cdd5bb39aa8f51e1
wangpengfei <wangpengfei@igetwell.cn>
2017-09-08 10:48:44 +0800

    我的项目

---
+++ okhttputils/src/main/java/com/jd/okhttputils/Text.java
@@ -0,0 +1,15 @@
+package com.jd.okhttputils;/**
+ * Created by jd on 2017/9/7.
+ */
+
+/**
+ * created by wangpengfei at 2017/9/7
+ */
+public class Text {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int delet(int a,int b){
+        return a-b;
+    }
+}
